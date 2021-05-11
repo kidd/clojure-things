36abd14e232efe94e1bde79c602a17e843847027
Raimon Grau <raimonster@gmail.com>
2021-05-11 09:38:58 +0100

    spec

---
+++ src/spec-things/main.clj
@@ -0,0 +1,5 @@
+;; https://practicalli.github.io/clojure/clojure-spec/
+;; https://www.pixelated-noise.com/blog/2020/09/10/what-spec-is/
+;; https://corfield.org/blog/2019/09/13/using-spec/
+
+(ns spec-things.main)
