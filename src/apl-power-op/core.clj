d55a2cef9848fdb7ca69de692e53e98e1b257445
Raimon Grau <raimonster@gmail.com>
2024-02-23 12:29:59 +0100

    stuff

--- src/apl-power-op/core.clj
+++ src/apl-power-op/core.clj
@@ -8,6 +8,17 @@
   #(take-while (complement pred)
                (partition 2 (iterate f %))))
 
+(defn power [pred f]
+  #(last
+    (last
+     (take-while (complement pred)
+                 (partition 2 (iterate f %))))))
+
 (last
  (last
   ((power #(good-enuf 0 (apply - %)) #(/ % 2)) 10)))
+
+(take 3 (iterate last ((power #(good-enuf 0 (apply - %)) #(/ % 2)) 10))) ; shows the path navigating the different structure depth
+(nth (iterate last ((power #(good-enuf 0 (apply - %)) #(/ % 2)) 10)) 2) ; takes the nth
+
+;; we can use power again
---
+++ src/combinator-things/core.clj
@@ -0,0 +1,63 @@
+(ns combinator-things.core)
+
+(defn apply-if
+  "if(p(m)) then f(m, ...args) else m
+
+  Useful for `maybe-*` like updates."
+  [m p f & args]
+  (if (p m)
+    (apply f m args)
+    m))
+
+(cond-> {:a 1}
+  true (update :a inc )
+  odd?)
+
+(-> {:a ["a b a"]}
+    (update :a apply-if string? str/replace #"a" "b")
+    (update :a apply-if vector? update 0 str/replace #"a" "b"))
+
+(-> {:a "a b a"}
+    (update :a apply-if string? str/replace #"a" "b")
+    (update :a apply-if vector? update 0 str/replace #"a" "b"))
+
+(-> {:a "a b a"}
+    (update :a apply-if string? vector)
+    (update :a apply-if vector? update 0 str/replace #"a" "b"))
+
+(-> {:a "a b a"}
+    (update :a apply-if string? vector)
+    (update :a update 0 str/replace #"a" "b"))
+
+(-> {:a "a b a"}
+    (update :a apply-if string? vector)
+    (update-in [:a 0] str/replace #"a" "b"))
+
+(with-redefs [jdbc/execute!
+              (fn [& args]
+                (apply old-jdbc-exec!
+                       (cond-> (vec args)
+                         (and (= :development (config/app-env)) (string? (get (vec args) 1)))
+                         (update 1 str/replace #"(?i)CREATE TABLE" "create unlogged table")
+                         (and (= :development (config/app-env)) (vector? (get (vec args) 1)))
+                         (update-in [1 0] str/replace #"(?i)CREATE TABLE" "create unlogged table"))))])
+
+                       (cond-> (vec args)
+                         (and (= :development (config/app-env)) (string? (get (vec args) 1)))
+                         (update 1 apply-if string? vector)
+                         (and (= :development (config/app-env)))
+                         (update-in [1 0] str/replace #"(?i)CREATE TABLE" "create unlogged table"))
+
+
+(-> (vec args)
+    (update 1 apply-if string? vector)
+    (update-in [1 0] str/replace #"(?i)CREATE TABLE" "create unlogged table"))
+
+(defn fork [l c r]
+  #(->> % ((juxt l r)) (apply c)))
+
+(defn avg [a]
+  ((fork (partial apply +) / count) a))
+
+(defn unique? [a]
+  ((fork count = (comp count set)) a))
